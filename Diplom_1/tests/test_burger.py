c635809bd9d8a4875e4491aa5066efe2604a5650
Julia Pozdnyakova <feainnemedd@yandex.ru>
2026-04-18 20:32:23 +0300

    Исправление по коментариям ревьюера

--- Diplom_1/tests/test_burger.py
+++ Diplom_1/tests/test_burger.py
@@ -1,8 +1,8 @@
 import allure
 import pytest
 from unittest.mock import Mock
-from praktikum.burger import Burger
-from data import ReceiptData, BurgerTestData
+from practicum.burger import Burger
+from data import IngredientData, BurgerTestData
 
 class TestBurger:
 
@@ -32,7 +32,7 @@ class TestBurger:
     @allure.title('Перемещение ингредиента в списке')
     def test_move_ingredient(self, mock_sauce, mock_filling):
         burger = Burger()
-        burger.add_ingredient(mock_sauce)  # index 0
+        burger.add_ingredient(mock_sauce)   # index 0
         burger.add_ingredient(mock_filling) # index 1
         burger.move_ingredient(0, 1)
         assert burger.ingredients[1] == mock_sauce
@@ -41,19 +41,38 @@ class TestBurger:
     @allure.title('Расчет итоговой стоимости бургера')
     def test_get_price(self, mock_bun, mock_sauce):
         burger = Burger()
-        burger.set_buns(mock_bun) # 100 * 2
-        burger.add_ingredient(mock_sauce) # + 100
-        # Ожидаем 300.0 (float)
-        assert burger.get_price() == 300.0
+        
+        
+        bun_price = 100.0
+        ingredient_price = 100.0
+        
+        mock_bun.get_price.return_value = bun_price
+        mock_sauce.get_price.return_value = ingredient_price
+        
+        burger.set_buns(mock_bun)
+        burger.add_ingredient(mock_sauce)
+        
+        expected_price = (bun_price * 2) + ingredient_price
+        
+        assert burger.get_price() == expected_price
 
     @allure.title('Проверка формирования чека')
     def test_get_receipt(self, mock_bun, mock_sauce):
         burger = Burger()
+        
+        mock_bun.get_name.return_value = "Red Bun"
+        mock_bun.get_price.return_value = 100.0
+        
+        mock_sauce.get_name.return_value = "Chili Sauce"
+        mock_sauce.get_type.return_value = "SAUCE"
+        mock_sauce.get_price.return_value = 100.0
+        
         burger.set_buns(mock_bun)
         burger.add_ingredient(mock_sauce)
         
         receipt = burger.get_receipt()
+        expected_price = (100.0 * 2) + 100.0
         
-        assert f"(==== {mock_bun.get_name()} ====)" in receipt
-        assert f"= {mock_sauce.get_type().lower()} {mock_sauce.get_name()} =" in receipt
-        assert "Price: 300" in receipt
+        assert f"(=== {mock_bun.get_name()} ===)" in receipt
+        assert f"- {mock_sauce.get_type().lower()} {mock_sauce.get_name()} -" in receipt
+        assert f"Price: {expected_price}" in receipt
